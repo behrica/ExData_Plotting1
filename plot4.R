bb5c262b142ad64cdf6102385e08b5cfc7e11207
Carsten Behring <carsten.behring@gmail.com>
2014-05-08 21:57:50 +0200

    decreased fomt size to to match reference

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@ source("load_data.R")
 
 png("plot4.png",height=480,width=480)
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2),cex=0.8)
 plot(consumption_subset$datetime,consumption_subset$Global_active_power,  type="l",xlab="",ylab="Global Active Power")
 
 plot(consumption_subset$datetime,consumption_subset$Voltage,type="l",xlab="datetime",ylab="Voltage")
@@ -10,7 +10,7 @@ plot(consumption_subset$datetime,consumption_subset$Voltage,type="l",xlab="datet
 plot(consumption_subset$datetime,consumption_subset$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
 points(consumption_subset$datetime,consumption_subset$Sub_metering_2,type="l",col="red")
 points(consumption_subset$datetime,consumption_subset$Sub_metering_3,type="l",col="blue")
-legend("topright",names(consumption_subset)[7:9],col=c("black","red","blue"),lwd=1)
+legend("topright",names(consumption_subset)[7:9],col=c("black","red","blue"),lwd=1,bty="n")
 
 
 plot(consumption_subset$datetime,consumption_subset$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
